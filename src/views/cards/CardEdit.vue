53325b1dd95d32fee022dcb71439af45d5b5595f
Tim Niehoff <tim.niehoff@b-tu.de>
2022-10-02 13:45:22 +0200

    feat: create flash card boilerplate

---
+++ src/views/cards/CardEdit.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="columns">
+      <div class="column is-8 is-offset-4">
+        <h1 class="title">Edit Card</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
