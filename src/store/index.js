7881c0ccace074a8a7dc44572e6bad08f0c95bac
Tim Niehoff <tim.niehoff@b-tu.de>
2022-10-02 09:40:40 +0200

    feat: prepare vuex for authentication via django

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: '',
+    isAuthenticated: false
   },
   getters: {
   },
   mutations: {
+    initStore(state) {
+      if (localStorage.getItem('token')) {
+        state.token = localStorage.getItem('token')
+        state.isAuthenticated = true
+      } else {
+        state.token = ''
+        state.isAuthenticated = false
+      }
+    },
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    deleteToken(state) {
+      state.token = ''
+      state.isAuthenticated = false
+    }
   },
   actions: {
   },
